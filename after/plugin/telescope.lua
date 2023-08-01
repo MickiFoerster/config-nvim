7d5fdf3acc7594e5fe26abb8ea300cedadeeeb56
micki <github@com-science.de>
2023-08-01 21:25:40 +0200

    telescope file browser

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,17 +1,15 @@
-
-
 -- You dont need to set any of these options. These are the default ones. Only
 -- the loading is important
 require('telescope').setup {
-  extensions = {
-    fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
+    extensions = {
+        fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
+        }
     }
-  }
 }
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
@@ -53,4 +51,12 @@ vim.keymap.set('n', "<leader>tw", builtin.lsp_workspace_symbols, {})
 --	builtin.grep_string({ search = vim.fn.expand("<cword>") });
 --end)
 
+-- telescope file browser:
+require("telescope").load_extension "file_browser"
 
+vim.api.nvim_set_keymap(
+    "n",
+    "<leader>fb",
+    ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+    { noremap = true }
+)
--- lua/micki/packer.lua
+++ lua/micki/packer.lua
@@ -4,77 +4,81 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
-	  -- or                            , branch = '0.1.x',
-	  requires = { 
-          {'nvim-lua/plenary.nvim'},
-          {'BurntSushi/ripgrep'}
-      }
-  }
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        -- or                            , branch = '0.1.x',
+        requires = {
+            { 'nvim-lua/plenary.nvim' },
+            { 'BurntSushi/ripgrep' }
+        }
+    }
+    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 
-  use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
-  })
+    use {
+        "nvim-telescope/telescope-file-browser.nvim",
+        requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+    }
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('nvim-treesitter/playground')
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
+    })
 
-  use('mbbill/undotree')
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use('nvim-treesitter/playground')
 
-  use('tpope/vim-fugitive')
+    use('mbbill/undotree')
 
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
+    use('tpope/vim-fugitive')
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
 
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},
-		  -- Snippet Collection (Optional)
-		  {'rafamadriz/friendly-snippets'},
-	  }
-  }
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
 
-  use('williamboman/mason.nvim')
-  use('williamboman/mason-lspconfig.nvim')
-  use('neovim/nvim-lspconfig')
-  use 'simrat39/rust-tools.nvim'
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            -- Snippet Collection (Optional)
+            { 'rafamadriz/friendly-snippets' },
+        }
+    }
 
-  -- Completion framework:
-  use 'hrsh7th/nvim-cmp'
+    use('williamboman/mason.nvim')
+    use('williamboman/mason-lspconfig.nvim')
+    use('neovim/nvim-lspconfig')
+    use 'simrat39/rust-tools.nvim'
 
-  -- LSP completion source:
-  use 'hrsh7th/cmp-nvim-lsp'
+    -- Completion framework:
+    use 'hrsh7th/nvim-cmp'
 
-  -- Useful completion sources:
-  use 'hrsh7th/cmp-nvim-lua'
-  use 'hrsh7th/cmp-nvim-lsp-signature-help'
-  use 'hrsh7th/cmp-vsnip'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/vim-vsnip'
+    -- LSP completion source:
+    use 'hrsh7th/cmp-nvim-lsp'
 
-  -- git blame info
-  use 'f-person/git-blame.nvim'
+    -- Useful completion sources:
+    use 'hrsh7th/cmp-nvim-lua'
+    use 'hrsh7th/cmp-nvim-lsp-signature-help'
+    use 'hrsh7th/cmp-vsnip'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/vim-vsnip'
 
+    -- git blame info
+    use 'f-person/git-blame.nvim'
 end)
